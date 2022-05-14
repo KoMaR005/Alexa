3c1c7652d73d5243ff612c2f71856921a81a2f54
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-05-14 21:10:47 +0530

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -1,9 +1,9 @@
-let {shefin} = require('../events');
+let Alexa = require('../events');
 let {MessageType} = require('@adiwajshing/baileys');
 let c = require('../config');
 let s = c.SUPPORT2
 let {upload} = require('alexa-bot');
-shefin({pattern: 'url ?(.*)', fromMe: true, desc:'Uploads image to imgur.com'}, (async (m, match) => { 
+Alexa.addCommand({pattern: 'url ?(.*)', fromMe: true, desc:'Uploads image to imgur.com'}, (async (m, match) => { 
 if (!m.reply_message.image && !m.reply_message.video ) return;
 var a = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
 var res = await upload(a,s)
