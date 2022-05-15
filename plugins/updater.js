d7fda04ca2208468ed200784474587b303199b68
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-05-15 11:39:37 +0530

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -14,47 +14,37 @@ Alexa.addCommand({pattern: 'update$', fromMe: true, desc: Lang.UPDATER_DESC}, (a
     await git.fetch();
     var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
     if (commits.total === 0) {
-        await message.sendReply(Lang.UPDATE);
+    await message.sendReply(Lang.UPDATE);
     } else {
-        var degisiklikler = Lang.NEW_UPDATE;
-        commits['all'].map(
-            (commit) => {
-                degisiklikler += '➥ [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' <' + commit.author_name + '>\n';
-            }
-        );
-        
-        await message.sendReply(degisiklikler + "```");
-    }
-}));
+    var degisiklikler = Lang.NEW_UPDATE;
+    commits['all'].map(
+    (commit) => {
+    degisiklikler += '➥ [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' <' + commit.author_name + '>\n';
+    });
+    await message.sendReply(degisiklikler + "```");
+    }}));
 
 Alexa.addCommand({pattern: 'update now$', fromMe: true, desc: Lang.UPDATE_NOW_DESC}, (async (message, match) => {
     await git.fetch();
     var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
     if (commits.total === 0) {
-        return await message.sendReply(Lang.UPDATE);
+    return await message.sendReply(Lang.UPDATE);
     } else {
-            await message.sendReply(Lang.UPDATING);
-            try {
-                var app = await heroku.get('/apps/' + Config.HEROKU.APP_NAME)
-            } catch {
-                return await message.sendReply(
-                    Lang.INVALID_HEROKU
-                   );
-            }
-
-            git.fetch('upstream', Config.BRANCH);
-            git.reset('hard', ['FETCH_HEAD']);
-
-            var git_url = app.git_url.replace(
-                "https://", "https://api:" + Config.HEROKU.API_KEY + "@"
-            )
-            
-            try {
-                await git.addRemote('heroku', git_url);
-            } catch { console.log('heroku remote ekli'); }
-            await git.push('heroku', Config.BRANCH);
-
-            await message.sendReply(Lang.UPDATED);
-            await message.sendReply(Lang.AFTER_UPDATE);
+    await message.sendReply(Lang.UPDATING);
+    try {
+         var app = await heroku.get('/apps/' + Config.HEROKU.APP_NAME)
+    } catch {
+    return await message.sendReply(Lang.INVALID_HEROKU);
     }
-}));
+    git.fetch('upstream', Config.BRANCH);
+    git.reset('hard', ['FETCH_HEAD']);
+    var git_url = app.git_url.replace(
+    "https://", "https://api:" + Config.HEROKU.API_KEY + "@"
+    )
+    try {
+    await git.addRemote('heroku', git_url);
+    } catch { console.log('heroku remote ekli'); }
+    await git.push('heroku', Config.BRANCH);
+    await message.sendReply(Lang.UPDATED);
+    await message.sendReply(Lang.AFTER_UPDATE);
+    }}));
