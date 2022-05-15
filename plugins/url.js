44bf010ee47c5d4de2189d15a0e9ef76193e3a75
5hefin! <104140006+5hefin@users.noreply.github.com>
2022-05-15 13:41:04 +0530

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -3,17 +3,21 @@ let {MessageType} = require('@adiwajshing/baileys');
 let c = require('../config');
 let s = c.SUPPORT2
 let {upload} = require('alexa-bot');
-let public = c.WORKTYPE == 'public' ? false : true
-let private = c.WORKTYPE == 'private' ? true : false
 
-Alexa.addCommand({pattern: 'url ?(.*)', fromMe: private, desc:'Uploads image to imgur.com'}, (async (m, match) => { 
+if (c.WORKTYPE == 'private') {
+
+  Alexa.addCommand({pattern: 'url ?(.*)', fromMe: true, desc:'Uploads image to imgur.com'}, (async (m, match) => { 
     if (!m.reply_message.image && !m.reply_message.video ) return;
     var a = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
     var res = await upload(a,s)
     await m.client.sendMessage(m.jid, res.link, MessageType.text, { quoted: m.data });}));
+}
 
-Alexa.addCommand({pattern: 'url ?(.*)', fromMe: public, desc:'Uploads image to imgur.com'}, (async (m, match) => { 
+else if (c.WORKTYPE == 'public') {
+
+  Alexa.addCommand({pattern: 'url ?(.*)', fromMe: true, desc:'Uploads image to imgur.com'}, (async (m, match) => { 
     if (!m.reply_message.image && !m.reply_message.video ) return;
     var a = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
     var res = await upload(a,s)
     await m.client.sendMessage(m.jid, res.link, MessageType.text, { quoted: m.data });}));
+}
